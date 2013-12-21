6f937a4271abbb18fcf01b09456d9609af2e1381
Kevan Ahlquist <ahlqu039@umn.edu>
2013-12-20 20:28:02 -0600

    Fixed root routing

--- ummbNet.py
+++ ummbNet.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+@app.route('/', defaults={'dummy': ''})
 @app.route("/<path:dummy>")
 def catchAll(dummy):
 	return render_template('ummbNet.html')
