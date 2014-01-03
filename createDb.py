d3831e052e1770fd35b3d7bb88a631eb6986940f
Kevan Ahlquist <ahlqu039@umn.edu>
2014-01-03 15:41:25 -0600

    Update createDb.py variable names
    No need to sacrifice readabilty for space.

--- createDb.py
+++ createDb.py
@@ -5,36 +5,37 @@ db.create_all()
 
 
 # Create bands
-goldband = Band('Gold Band')
-maroonband = Band('Maroon Band')
-gopherband = Band('Gopher Band')
+gold_band = Band('Gold Band')
+maroon_band = Band('Maroon Band')
+gopher_band = Band('Gopher Band')
 
-db.session.add_all([goldband, maroonband, gopherband])
+db.session.add_all([gold_band, maroon_band, gopher_band])
 db.session.commit()
 
 # Create EventTypes
-mbb = EventType("Men's Basketball")
-wbb = EventType("Women's Basketball")
-mh = EventType("Men's Hockey")
-wh = EventType("Women's Hockey")
-vb = EventType("Volleyball")
+mens_basketball = EventType("Men's Basketball")
+womens_basketball = EventType("Women's Basketball")
+mens_hockey = EventType("Men's Hockey")
+womens_hockey = EventType("Women's Hockey")
+volleyball = EventType("Volleyball")
 
-db.session.add_all([mbb, wbb, mh, wh, vb])
+db.session.add_all([mens_basketball, womens_basketball, mens_hockey, \
+                    womens_hockey, volleyball])
 db.session.commit()
 
 # Create Instruments
-pic = Instrument('Piccolo')
-flt = Instrument('Flute')
-clar = Instrument('Clarinet')
-altsax = Instrument('Alto Sax')
-tensax = Instrument('Tenor Sax')
-tpt = Instrument('Trumpet')
-melo = Instrument('Mellophone')
-tbone = Instrument('Trombone')
-bari = Instrument('Baritone')
+piccolo = Instrument('Piccolo')
+flute = Instrument('Flute')
+clarinet = Instrument('Clarinet')
+alto_sax = Instrument('Alto Sax')
+tenor_sax = Instrument('Tenor Sax')
+trumpet = Instrument('Trumpet')
+mellophone = Instrument('Mellophone')
+trombone = Instrument('Trombone')
+baritone = Instrument('Baritone')
 tuba = Instrument('Tuba')
-dline = Instrument('Drumline')
+drumline = Instrument('Drumline')
 
-db.session.add_all([pic, flt, clar, altsax, tensax, \
-                    tpt, melo, tbone, bari, tuba, dline])
+db.session.add_all([piccolo, flute, clarinet, alto_sax, tenor_sax, \
+                    trumpet, mellophone, trombone, baritone, tuba, drumline])
 db.session.commit()
