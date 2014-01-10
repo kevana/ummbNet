ae067c9b0ec37db6327234b6cc55686d8b345455
Kevan Ahlquist <ahlqu039@umn.edu>
2014-01-09 23:48:54 -0600

    Added instruments to test data import

--- ummbNet.py
+++ ummbNet.py
@@ -40,7 +40,8 @@ class User(db.Model):
     enabled = db.Column(db.Boolean)
 
     def __init__(self, username, email, password, first_name=None, \
-                last_name=None, nickname=None, requests=None, enabled=True):
+                last_name=None, nickname=None, requests=None, enabled=True, \
+                instruments=None):
         self.username = username
         self.email = email
         self.pw_hash = bcrypt.generate_password_hash(password)
@@ -52,6 +53,8 @@ class User(db.Model):
             self.nickname = nickname
         if requests:
             self.requests = requests
+        if instruments:
+            self.instruments = instruments
         self.enabled = enabled
 
     def __repr__(self):
@@ -224,7 +227,11 @@ def users():
 @login_required
 def user(username):
     '''Route to a particular user.'''
-    return 'User: %s' % username # render_template('user.html', username=username)
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template('user.html', user=user, \
+                            requests=user.requests, instruments=user.instruments)
+    return render_template('404.html')
 
 @app.route('/newuser', methods=['GET', 'POST'])
 def newuser():
