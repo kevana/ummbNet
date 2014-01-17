788f14965e636e6ade3c25ab58563d1ac2e8b357
Kevan Ahlquist <ahlqu039@umn.edu>
2014-01-16 19:36:16 -0600

    Added simple async wrapper using threads

---
+++ async.py
@@ -0,0 +1,11 @@
+'''
+Async functions for ummbNet
+'''
+
+from threading import Thread
+
+def async(f):
+    def wrapper(*args, **kwargs):
+        thr = Thread(target = f, args = args, kwargs = kwargs)
+        thr.start()
+    return wrapper
