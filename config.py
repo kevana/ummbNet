1c1ce6201cc1240ab834c7f2eb3ed6508afa359d
Kevan Ahlquist <ahlqu039@umn.edu>
2014-01-16 19:58:12 -0600

    Added email config properties

--- config.py
+++ config.py
@@ -5,8 +5,19 @@ Contains configuration directives for Flask and its plugins
 # Flask.session
 SECRET_KEY='Shhhh, this is a secret'
 
-# SQLAlchemy
+# Flask-SQLAlchemy
 SQLALCHEMY_DATABASE_URI='sqlite:////tmp/test.db'
+
+# Flask-Mail
+MAIL_SERVER = 'smtp.example.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'noreply@example.com'
+MAIL_PASSWORD = 'examplePassword'
+
+# administrator list
+ADMINS = ['admin@example.com']
 
 # Development
 DEBUG=True
