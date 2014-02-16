b5428f3678827c07f0b8409e218c24ec387cb476
Kevan Ahlquist <ahlqu039@umn.edu>
2014-02-15 23:52:07 -0600

    Changed to Individual notification emails
    Updated test data, fixed favicon.ico in base template

--- emails.py
+++ emails.py
@@ -14,13 +14,9 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(subject, recipients, text_body, html_body, sender=None):
+def send_email(subject, text_body, html_body, sender=None, recipients=None, bcc=None):
     '''Construct and send an email message.'''
-    if sender:
-        msg = Message(subject, sender=sender, recipients=recipients)
-    else:
-        msg = Message(subject, recipients=recipients)
-
+    msg = Message(subject, sender=sender, recipients=recipients, bcc=bcc)
     msg.body = text_body
     msg.html = html_body
 
@@ -59,11 +55,23 @@ def send_new_req_emails(req):
     # Send notification to subscribed users if list is not empty
     if req.instrument.notify_users_add:
         subject = 'New %s sub request posted' % req.instrument.name
-        msg_to = [user.email for user in req.instrument.notify_users_add]
         txt_body = render_template('email/req_add_notify_email.txt', req=req)
         html_body = render_template('email/req_add_notify_email.html', req=req)
-        send_email(subject=subject, recipients=msg_to,
-                   text_body=txt_body, html_body=html_body)
+        send_new_req_notif_emails(req=req, subject=subject,
+                                    txt_body=txt_body, html_body=html_body)
+
+@async
+def send_new_req_notif_emails(req, subject, txt_body, html_body):
+    '''Send notifications to subscribed users in a single connection.'''
+    with app.app_context():
+        with mail.connect() as conn:
+            for user in req.instrument.notify_users_add:
+                if user != req.poster:
+                    msg = Message(subject=subject,
+                                    recipients=[user.email],
+                                    body=txt_body,
+                                    html=html_body)
+                    conn.send(msg)
 
 def send_req_pickup_emails(req):
     '''Send pickup notifications to the poster and sub of a request.'''
